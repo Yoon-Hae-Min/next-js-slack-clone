76ffe551fecc2ff919765f7e360f43eceee43806
Yoon-Hae-Min <yunhatmi@naver.com>
2023-01-31 00:49:30 +0900

    feat: 로그인 페이지 디자인 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        focusInput: 'shadow-[0_0_0_1px_rgba(29,155,209,1),0_0_0_5px_rgba(29,155,209,0.3)]',
+      },
+    },
   },
   plugins: [],
 };
