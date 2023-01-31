25dc723075f65dcd79a2cc8bf1955355bd16349e
Yoon-Hae-Min <yunhatmi@naver.com>
2023-01-31 13:49:32 +0900

    refactor: AuthorizationInput 컴포넌트 생성

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import AuthorizationInput from '@components/AuthorizationInput';
 
 const SignUp = () => {
   return (
@@ -9,37 +10,22 @@ const SignUp = () => {
         <label id="email-label" className="block cursor-pointer pb-1 text-left text-sm font-bold">
           <span className="mb-1 block">이메일 주소</span>
           <div>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="mb-5 box-border h-11 w-full rounded 
-              border-[1px] border-solid border-[rgba(134,134,134,1)] 
-              bg-[rgba(255,255,255)] pt-3 pb-4 text-lg leading-4 
-              focus:shadow-focusInput"
-            />
+            <AuthorizationInput type="email" id="email" name="email" />
           </div>
         </label>
         <label id="password-label" className="block cursor-pointer pb-1 text-left text-sm font-bold">
           <span className="mb-1 block">비밀번호</span>
           <div>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="mb-5 box-border h-11 w-full rounded 
-              border-[1px] border-solid border-[rgba(134,134,134,1)] 
-              bg-[rgba(255,255,255)] pt-3 pb-4 text-lg leading-4 
-              focus:shadow-focusInput"
-            />
+            <AuthorizationInput type="password" id="password" name="password" />
           </div>
         </label>
         <button
           type="submit"
-          className="shadow-[0_1px_4px_rgba(0, 0, 0, 0.3)] mb-3
-          h-11 w-full max-w-full cursor-pointer rounded bg-[#4a154b] pb-2 pr-4 pl-1
+          className="shadow-[0_1px_4px_rgba(0, 0, 0, 0.3)] my-3
+          h-11 w-full max-w-full cursor-pointer rounded bg-[#4a154b]
           text-lg font-bold text-white 
-          hover:bg-[rgba(74,21,75,0.9)] 
+          transition-all duration-75 ease-linear
+          hover:bg-[rgba(74,21,75,0.9)]
           focus:shadow-[0_0_0_5px_rgba(18,100,163,1),0_0_0_5px_rgba(29,155,209,0.3)]
           "
         >
